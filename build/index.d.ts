c98f42d4205c2104ff766a0e396faded2c784b5a
vsudakov <sudakovmail@gmail.com>
2017-10-18 17:17:27 +0300

    fixed typings.

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,8 @@
-export * from './baseClasses/index';
-export * from './components/index';
-export * from './constants/index';
-export * from './enums/index';
-export * from './exceptions/index';
-export * from './models/index';
-export * from './services/index';
+import * as BaseClasses from './baseClasses/index';
+import * as Components from './components/index';
+import * as Constants from './constants/index';
+import * as Enums from './enums/index';
+import * as Exceptions from './exceptions/index';
+import * as Models from './models/index';
+import * as Services from './services/index';
+export { BaseClasses, Components, Constants, Enums, Exceptions, Models, Services };
